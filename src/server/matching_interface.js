f350382276fe9d98caf85911931cf41080a037bf
sabinekoch <sabine@allyapp.com>
2015-06-15 16:16:48 +0200

    add subId for files with > 1 feature

--- src/server/matching_interface.js
+++ src/server/matching_interface.js
@@ -2,14 +2,15 @@ var matchTrace = require('../match_trace.js');
 
 module.exports = function(app, db, osrm) {
   app.get('/match/:id', function(req, res) {
-    var id = parseInt(req.params.id);
+    var id = parseInt(req.params.id),
+        subId = parseInt(req.params.subId);
 
     db.get("SELECT file FROM traces WHERE id = ? LIMIT 1", id, function(err, row) {
       if (err) {
         res.send(JSON.stringify({status: "error", message: err.message}));
         return;
       }
-      matchTrace(osrm, row.file, function(err, result) {
+      matchTrace(subId, osrm, row.file, function(err, result) {
         if (err) {
           res.send(JSON.stringify({status: "error", message: err.message}));
           return;
